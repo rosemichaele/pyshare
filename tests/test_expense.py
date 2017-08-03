2ac9fecca228f4dce578c38c7f055f94ff518e37
Michael Rose <michaelrose126@gmail.com>
2017-08-03 12:41:15 -0400

    Add tests for adding parties to expenses

--- tests/test_expense.py
+++ tests/test_expense.py
@@ -1,7 +1,20 @@
+import pytest
 from pyshare.expense import Expense
+from pyshare.party import Party
 
 
 class TestExpense:
+    @classmethod
+    def setup_class(cls):
+        cls.not_a_party = "This is not a party.Party, it's a string."
+        cls.test_party = Party(name="Michael")
+        cls.test_expense = Expense(paid_for="Impulse purchase", currency="USD", amount=153.19)
 
-    def placeholder(self):
-        return True
+    def test_add_party_to_expense_succeeds(self):
+        assert not self.test_expense.parties_involved
+        self.test_expense.add_party(self.test_party)
+        assert len(self.test_expense.parties_involved) == 1
+
+    def test_add_not_party_to_expense_fails(self):
+        with pytest.raises(AssertionError):
+            self.test_expense.add_party(self.not_a_party)
